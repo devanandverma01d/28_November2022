64a8057caeca25963a820189c594c4c1d54c7242
Devanand Verma <devanandverma01d@gmail.come>
2023-01-01 23:33:52 +0530

    new

---
+++ js/script1.js
@@ -0,0 +1,1 @@
+console.log("This is code from External js file");
---
+++ js/script3.js
@@ -0,0 +1,6 @@
+function turnon() {
+    document.getElementById('demo').src = "./image/2.gif";
+}
+function turnoff() {
+    document.getElementById('demo').src = "./image/1.gif";
+}
